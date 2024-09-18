d8380540dceefa46e6ca8422ab733a1498e8a316
brown-cookies <jesimielalmedejar1018@gmail.com>
2024-09-18 10:44:18 +0800

    feat: set and add basic boilerplate for login and register

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,11 +2,17 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("", views.index_page, name="index"),
+    path("account/", include("account.urls")),
+    path("blood/", include("blood.urls")),
+    path("dashboard/", include("dashboard.urls")),
     path("admin/", admin.site.urls),
+    path("login/", views.login_page, name="login"),
+    path("logout/", views.logout_page, name="logout"),
+    path("register/", views.register_page, name="register"),
 ]
 
 if settings.DEBUG:
--- quiz/views.py
+++ quiz/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index_page(request: HttpRequest):
     return render(request, "pages/index.html")
+
+def login_page(request: HttpRequest):
+    return render(request, "pages/login.html")
+
+def logout_page(request: HttpRequest):
+    return render(request, "pages/logout.html")
+
+def register_page(request: HttpRequest):
+    return render(request, "pages/register.html")
